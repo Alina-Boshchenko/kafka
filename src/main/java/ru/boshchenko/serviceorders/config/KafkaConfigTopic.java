48eec28a908358975fc45bee2a78578a1368c2b6
AlinaBoschenko <alina.xalimova14@gmail.com>
2025-04-08 00:36:52 +0300

    ver_1.0.1

--- src/main/java/ru/boshchenko/serviceorders/config/KafkaConfigTopic.java
+++ src/main/java/ru/boshchenko/serviceorders/config/KafkaConfigTopic.java
@@ -2,6 +2,7 @@ package ru.boshchenko.serviceorders.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -12,12 +13,21 @@ import java.util.Map;
 @Slf4j
 public class KafkaConfigTopic {
 
+    @Value("${spring.kafka.topics.new_orders.name}")
+    private String topicName;
+
+    @Value("${spring.kafka.topics.new_orders.partitions}")
+    private int partitions;
+
+    @Value("${spring.kafka.topics.new_orders.replication-factor}")
+    private int replicas;
+
     @Bean
     public NewTopic createTopic(){
         log.info("СОЗДАН ТОПИК 'new_orders'");
-        return TopicBuilder.name("new_orders")
-                .partitions(3)
-                .replicas(3)
+        return TopicBuilder.name(topicName)
+                .partitions(partitions)
+                .replicas(replicas)
                 .configs(Map.of("min.insync.replicas", "2"))
                 .build();
     }
